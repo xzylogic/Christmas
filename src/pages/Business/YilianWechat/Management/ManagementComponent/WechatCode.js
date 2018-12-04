ad899d954ae988a107345f3569ea555e3abea30f
xling <516032842@qq.com>
2018-12-04 13:57:50 +0800

    修改人员管理中推广码

--- src/pages/Business/YilianWechat/Management/ManagementComponent/WechatCode.js
+++ src/pages/Business/YilianWechat/Management/ManagementComponent/WechatCode.js
@@ -3,33 +3,11 @@ import { Modal } from 'antd';
 
 class WechatCode extends Component {
   handleOk = e => {
-    console.log(e);
-
-    // var a = document.createElement('a');
-    // a.href = "http://www.wwei.cn/qrcode-viewfile?type=qrcode_text&k=qN3Xz&hash=31d84c9c3a2e1e8d37e191a5ec90d648&timeout=1543827340&size=100";
-    // a.click();
-    // var a = document.createElement('a');
-    // // 创建一个单击事件
-    // var event = new MouseEvent('click');
-
-    // // 将a的download属性设置为我们想要下载的图片名称，若name不存在则使用‘下载图片名称’作为默认名称
-    // a.download = name || '下载图片名称';
-    // // 将生成的URL设置为a.href属性
-    // a.href =
-    //   'http://www.wwei.cn/qrcode-viewfile?type=qrcode_text&k=qN3Xz&hash=31d84c9c3a2e1e8d37e191a5ec90d648&timeout=1543827340&size=100';
-
-    // // 触发a的单击事件
-    // a.dispatchEvent(event);
-
-    // this.setState({
-    //   visible: true,
-    // });
+    console.log('handleOk', e);
   };
 
   render() {
-    const { showCode, onClose } = this.props;
-
-    // console.log(this.props)
+    const { showCode, onClose, imgUrl } = this.props;
     return (
       <div>
         <Modal
@@ -38,14 +16,15 @@ class WechatCode extends Component {
           centered
           onCancel={onClose}
           onOk={this.handleOk}
+          footer={null}
         >
-          {
+          {/* {
             <img
               src="http://www.wwei.cn/qrcode-viewfile?type=qrcode_text&k=qN3Xz&hash=31d84c9c3a2e1e8d37e191a5ec90d648&timeout=1543827340&size=100"
               alt=""
             />
-          }
-          {/* <img src={imgUrl} alt="" /> */}
+          } */}
+          <img src={imgUrl} alt="" style={{ display: 'block', margin: '0 auto' }} />
         </Modal>
       </div>
     );

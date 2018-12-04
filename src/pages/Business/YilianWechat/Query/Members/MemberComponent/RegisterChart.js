7b6e806d39374a48dd1723b980d7d3e1ff2b5542
VioletQQY <forviolet771@gmail.com>
2018-12-04 14:57:00 +0800

    Member Query

---
+++ src/pages/Business/YilianWechat/Query/Members/MemberComponent/RegisterChart.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import createG2 from 'g2-react';
+
+const Line = createG2(chart => {
+  chart.col('date', { alias: ' ' });
+  chart.col('value', { alias: '注册人数' });
+  chart.col('type', { alias: '渠道' });
+  chart
+    .line()
+    .position('date*value')
+    .color('type', ['#36cfc9', '#ffc53d', '#40a9ff', '#ff4d4f'])
+    .shape('line')
+    .size(2);
+  chart.render();
+});
+
+const getType = type => {
+  switch (type) {
+    case 'online':
+      return '微信线上注册数';
+    case 'person':
+      return '微信地推人员注册数';
+    case 'hos':
+      return '微信医院二维码注册数';
+    case 'counts':
+      return '微信注册总数';
+    default:
+      return '';
+  }
+};
+
+const getDateSource = data =>
+  data.filter(obj => obj.date !== undefined).reduce((preArr, currObj) => {
+    const singleArr = Object.keys(currObj)
+      .filter(key => !(key === 'date' || key === 'appName'))
+      .reduce((pre, curr, i, arr) => {
+        const singleObj = {
+          date: currObj.date,
+          appName: currObj.appName,
+          type: getType(arr[i]),
+          value: currObj[arr[i]],
+        };
+        return [...pre, ...[singleObj]];
+      }, []);
+    return [...preArr, ...singleArr];
+  }, []);
+
+class FollowChart extends Component {
+  state = {
+    width: 1000,
+    height: 500,
+    plotCfg: {
+      margin: [10, 100, 200, 120],
+    },
+  };
+
+  render() {
+    const { width, height, plotCfg } = this.state;
+    const { data } = this.props;
+
+    return (
+      <Line
+        data={(data && getDateSource(data)) || []}
+        width={width}
+        height={height}
+        plotCfg={plotCfg}
+        forceFit
+      />
+    );
+  }
+}
+
+export default FollowChart;
--- src/pages/Business/YilianWechat/Query/Members/Members.js
+++ src/pages/Business/YilianWechat/Query/Members/Members.js
@@ -1,12 +1,266 @@
 import React, { Component } from 'react';
+import { connect } from 'dva';
+import { Tabs, Radio, Table, Button } from 'antd';
+import debounce from 'lodash.debounce';
 
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import MemberSearch from './MemberComponent/MemberSearchBar';
+import FollowChart from './MemberComponent/FollowChart';
+import RegisterChart from './MemberComponent/RegisterChart';
 
-export class Member extends Component {
-  componentDidMount() {}
+import classes from './Members.less';
+
+const mapStateToProps = state => ({
+  searchParam: state.businessYilianWechatQuery.searchParam.membership,
+  followingList: state.businessYilianWechatQuery.list.following,
+  registrationList: state.businessYilianWechatQuery.list.registration,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onFetchMembershipList: () =>
+    dispatch({
+      type: 'businessYilianWechatQuery/fetchMembership',
+    }),
+  onFetchMembershipListDebounce: debounce(
+    page =>
+      dispatch({
+        type: 'businessYilianWechatQuery/fetchMembership',
+        payload: { page },
+      }),
+    500
+  ),
+  onSearchParamChange: (key, value) =>
+    dispatch({
+      type: 'businessYilianWechatQuery/updateSearchParam',
+      payload: { origin: 'membership', key, value },
+    }),
+});
+
+const renderContent = (text, record, index, listLength) => {
+  let content = '';
+  if (index === listLength - 1) {
+    content = <span style={{ color: 'red' }}>{text}</span>;
+  } else {
+    content = text;
+  }
+  return content;
+};
+
+@connect(
+  mapStateToProps,
+  mapDispatchToProps
+)
+class Member extends Component {
+  state = {
+    tab1Show: 'chart',
+    tab2Show: 'chart',
+  };
+
+  componentDidMount() {
+    const { onFetchMembershipList } = this.props;
+    onFetchMembershipList();
+  }
+
+  hangdleTab1Change = e => {
+    this.setState({ tab1Show: e.target.value });
+  };
+
+  hangdleTab2Change = e => {
+    this.setState({ tab2Show: e.target.value });
+  };
+
+  setFTableColumns = () => {
+    const { followingList } = this.props;
+    const listLength = (followingList && followingList.length) || 0;
+    const columns = [
+      {
+        title: '日期',
+        dataIndex: 'date',
+        key: 'date',
+      },
+      {
+        title: '机构',
+        dataIndex: 'appName',
+        key: 'appName',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信线上关注数',
+        dataIndex: 'online',
+        key: 'online',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信地推人员关注数',
+        dataIndex: 'person',
+        key: 'person',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信医院二维码关注数',
+        dataIndex: 'hos',
+        key: 'hos',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信关注总数',
+        dataIndex: 'counts',
+        key: 'counts',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信取消关注数',
+        dataIndex: 'unFollow',
+        key: 'unFollow',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+    ];
+    return columns;
+  };
+
+  setRTableColumns = () => {
+    const { registrationList } = this.props;
+    const listLength = (registrationList && registrationList.length) || 0;
+    const columns = [
+      {
+        title: '日期',
+        dataIndex: 'date',
+        key: 'date',
+      },
+      {
+        title: '机构',
+        dataIndex: 'appName',
+        key: 'appName',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信线上注册数',
+        dataIndex: 'online',
+        key: 'online',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信地推人员注册数',
+        dataIndex: 'person',
+        key: 'person',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信医院二维码注册数',
+        dataIndex: 'hos',
+        key: 'hos',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+      {
+        title: '微信注册总数',
+        dataIndex: 'counts',
+        key: 'counts',
+        render: (text, record, index) => renderContent(text, record, index, listLength),
+      },
+    ];
+    return columns;
+  };
+
+  getList = list => {
+    const listCopy = [...list];
+    const listCounts = listCopy.reduce((preObj, obj) => ({
+      counts: Number(preObj.counts + obj.counts),
+      hos: Number(preObj.hos + obj.hos),
+      online: Number(preObj.online + obj.online),
+      person: Number(preObj.person + obj.person),
+      unFollow: Number(preObj.unFollow + obj.unFollow),
+    }));
+    listCopy.push(listCounts);
+    return listCopy;
+  };
+
+  handleParamsChange = async (value, dataKey) => {
+    const { onSearchParamChange, onFetchMembershipListDebounce } = this.props;
+    if (dataKey === 'date') {
+      await onSearchParamChange('startTime', value[0]);
+      await onSearchParamChange('endTime', value[1]);
+    } else {
+      await onSearchParamChange(dataKey, value);
+    }
+    await onFetchMembershipListDebounce(0);
+  };
+
+  handleFollowDownload = e => {
+    e.preventDefault();
+    console.log('follow download');
+  };
+
+  handleRegisterDownload = e => {
+    e.preventDefault();
+    console.log('register download');
+  };
 
   render() {
-    return <PageHeaderWrapper>Member</PageHeaderWrapper>;
+    const { followingList, registrationList, searchParam } = this.props;
+    const { tab1Show, tab2Show } = this.state;
+    return (
+      <PageHeaderWrapper>
+        <div className={classes.Container}>
+          <MemberSearch params={searchParam} onParamsChange={this.handleParamsChange} />
+          <Tabs className={classes.Content} animated={false}>
+            <Tabs.TabPane tab="会员关注" key="1">
+              <div className={classes.Map}>
+                <Radio.Group
+                  className={classes.Gap}
+                  onChange={this.hangdleTab1Change}
+                  defaultValue={tab1Show}
+                >
+                  <Radio.Button value="chart">图形</Radio.Button>
+                  <Radio.Button value="list">列表</Radio.Button>
+                </Radio.Group>
+                <Button type="ghost" onClick={this.handleFollowDownload}>
+                  下载
+                </Button>
+              </div>
+              {tab1Show === 'list' ? (
+                <div>
+                  <Table
+                    rowKey={(_, index) => index}
+                    columns={this.setFTableColumns()}
+                    dataSource={followingList}
+                    pagination={false}
+                  />
+                </div>
+              ) : (
+                <FollowChart data={followingList} />
+              )}
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="会员注册" key="2">
+              <div className={classes.Map}>
+                <Radio.Group
+                  className={classes.Gap}
+                  onChange={this.hangdleTab2Change}
+                  defaultValue={tab2Show}
+                >
+                  <Radio.Button value="chart">图形</Radio.Button>
+                  <Radio.Button value="list">列表</Radio.Button>
+                </Radio.Group>
+                <Button type="ghost" onClick={this.handleRegisterDownload}>
+                  下载
+                </Button>
+              </div>
+              {tab2Show === 'list' ? (
+                <div>
+                  <Table
+                    rowKey={(_, index) => index}
+                    columns={this.setRTableColumns()}
+                    dataSource={registrationList}
+                    pagination={false}
+                  />
+                </div>
+              ) : (
+                <RegisterChart data={registrationList} />
+              )}
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
+      </PageHeaderWrapper>
+    );
   }
 }
 

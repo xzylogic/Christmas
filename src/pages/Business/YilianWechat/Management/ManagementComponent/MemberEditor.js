fc4e834dca0740a6adbd35bf8982b04807d9b539
xinglingling <516032842@qq.com>
2019-01-02 18:11:01 +0800

    010204modify

--- src/pages/Business/YilianWechat/Management/ManagementComponent/MemberEditor.js
+++ src/pages/Business/YilianWechat/Management/ManagementComponent/MemberEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Form, Modal, Input, Select, Checkbox } from 'antd';
+import { Form, Modal, Input, Select, Checkbox, DatePicker } from 'antd';
+import moment from 'moment';
 
 const mapStateToProps = state => ({
   createLoading: state.loading.effects['businessYilianWechatManagement/createMember'],
@@ -23,12 +24,24 @@ const mapDispatchToProps = dispatch => ({
     }),
 });
 
+const getSecondsDisabled = num => {
+  const secDisableArr = [];
+  for (let sec = 1; sec < num; sec += 1) {
+    secDisableArr.push(sec);
+  }
+  return secDisableArr;
+};
+
 @connect(
   mapStateToProps,
   mapDispatchToProps
 )
 @Form.create()
 class MemberEditor extends Component {
+  state = {
+    haveDefaultSite: true,
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     const { form, onCreateMember, onModifyMember, onClose, initialValue } = this.props;
@@ -42,6 +55,7 @@ class MemberEditor extends Component {
           groupId: values.groupName,
           site: values.site,
           promoCode: values.promoCode.join(' '),
+          effectiveDate: values.effectiveDate.format('YYYY-MM-DD HH:mm'),
         };
         onCreateMember(postData).then(ifsuccess => {
           if (ifsuccess) {
@@ -58,6 +72,7 @@ class MemberEditor extends Component {
           groupName: values.groupName,
           site: values.site,
           promoCode: values.promoCode.join(' '),
+          effectiveDate: values.effectiveDate.format('YYYY-MM-DD HH:mm'),
         };
         onModifyMember(postData).then(ifsuccess => {
           if (ifsuccess) {
@@ -66,6 +81,26 @@ class MemberEditor extends Component {
         });
       }
     });
+    this.setState({ haveDefaultSite: true });
+  };
+
+  handleChangeGroupAdd = value => {
+    const { onParamChange, form } = this.props;
+    onParamChange('hosGroupName', value);
+    form.resetFields('site', '');
+  };
+
+  handleChangeGroupEditor = value => {
+    this.setState({ haveDefaultSite: false });
+    const { onParamChange, form } = this.props;
+    onParamChange('recordGroupName', value);
+    form.resetFields('site', '');
+  };
+
+  handleCancle = () => {
+    const { onClose } = this.props;
+    this.setState({ haveDefaultSite: true });
+    onClose();
   };
 
   render() {
@@ -75,13 +110,12 @@ class MemberEditor extends Component {
       initialValue,
       createLoading,
       modifyLoading,
-      onClose,
       getMessage,
       groupHosNameList,
       groupHosNameEditorList,
-      onParamChange,
       form: { getFieldDecorator },
     } = this.props;
+    const { haveDefaultSite } = this.state;
 
     const formItemLayout = {
       labelCol: {
@@ -94,12 +128,64 @@ class MemberEditor extends Component {
         md: { span: 18 },
       },
     };
+
     const options = [
       { label: '医联微信', value: '医联微信' },
       { label: '支付宝', value: '支付宝' },
       { label: 'APP', value: 'APP' },
       { label: '健康云', value: '健康云' },
     ];
+    const secDisableArr = getSecondsDisabled(60);
+
+    function disabledDateTime() {
+      return {
+        disabledSeconds: () => secDisableArr,
+      };
+    }
+
+    const EditorSite = () => {
+      let content = '';
+      if (groupHosNameEditorList instanceof Object && groupHosNameEditorList.length) {
+        if (haveDefaultSite) {
+          content = (
+            <Form.Item {...formItemLayout} label="推广地址">
+              {getFieldDecorator('site', {
+                initialValue:
+                  (initialValue && initialValue.site) || groupHosNameEditorList[0].hos_name,
+                rules: [{ required: true, message: '请选择推广地址！' }],
+              })(
+                <Select placeholder="请选择推广地址">
+                  {groupHosNameEditorList.map(item => (
+                    <Select.Option id={item.id} key={item.id} value={item.hos_name}>
+                      {item.hos_name}
+                    </Select.Option>
+                  ))}
+                </Select>
+              )}
+            </Form.Item>
+          );
+        } else {
+          content = (
+            <Form.Item {...formItemLayout} label="推广地址">
+              {getFieldDecorator('site', {
+                initialValue:
+                  groupHosNameEditorList[0].hos_name || (initialValue && initialValue.site),
+                rules: [{ required: true, message: '请选择推广地址！' }],
+              })(
+                <Select placeholder="请选择推广地址">
+                  {groupHosNameEditorList.map(item => (
+                    <Select.Option id={item.id} key={item.id} value={item.hos_name}>
+                      {item.hos_name}
+                    </Select.Option>
+                  ))}
+                </Select>
+              )}
+            </Form.Item>
+          );
+        }
+      }
+      return content;
+    };
 
     return (
       <Modal
@@ -109,7 +195,8 @@ class MemberEditor extends Component {
         onOk={this.handleSubmit}
         okButtonProps={{ htmlType: 'submit' }}
         confirmLoading={createLoading || modifyLoading}
-        onCancel={onClose}
+        // onCancel={onClose}
+        onCancel={this.handleCancle}
       >
         {visible ? (
           <Form>
@@ -140,11 +227,26 @@ class MemberEditor extends Component {
                     rules: [{ required: true, message: '请填写工号！' }],
                   })(<Input placeholder="请填写工号" />)}
                 </Form.Item>
+
+                <Form.Item {...formItemLayout} label="生效时间">
+                  {getFieldDecorator('effectiveDate', {
+                    rules: [{ required: true, message: '请选择生效时间！' }],
+                  })(
+                    <DatePicker
+                      format="YYYY-MM-DD HH:mm"
+                      disabledTime={disabledDateTime}
+                      showToday={false}
+                      showTime={{ defaultValue: moment('09:00', 'HH:mm') }}
+                      setFieldsValue={moment(new Date(), 'YYYY-MM-DD')}
+                    />
+                  )}
+                </Form.Item>
                 <Form.Item {...formItemLayout} label="所属小组">
                   {getFieldDecorator('groupName', {
                     rules: [{ required: true, message: '请选择小组！' }],
+                    resetFields: {},
                   })(
-                    <Select onChange={value => onParamChange('hosGroupName', value)}>
+                    <Select onChange={this.handleChangeGroupAdd}>
                       {getMessage.groups.map(item => (
                         <Select.Option id={item.id} key={item.id} value={item.id}>
                           {item.name}
@@ -190,7 +292,7 @@ class MemberEditor extends Component {
                     initialValue: (initialValue && initialValue.groupName) || '',
                     rules: [{ required: true, message: '请选择小组！' }],
                   })(
-                    <Select onChange={value => onParamChange('recordGroupName', value)}>
+                    <Select onChange={this.handleChangeGroupEditor}>
                       {getMessage.groups.map(item => (
                         <Select.Option id={item.id} key={item.id} value={item.id}>
                           {item.name}
@@ -199,12 +301,12 @@ class MemberEditor extends Component {
                     </Select>
                   )}
                 </Form.Item>
-
-                {groupHosNameEditorList instanceof Object && groupHosNameEditorList.length ? (
+                {EditorSite()}
+                {/* {groupHosNameEditorList instanceof Object && groupHosNameEditorList.length ? (
                   <Form.Item {...formItemLayout} label="推广地址">
                     {getFieldDecorator('site', {
                       initialValue:
-                        (initialValue && initialValue.site) || groupHosNameEditorList[0].hos_name,
+                         groupHosNameEditorList[0].hos_name || (initialValue && initialValue.site),
                       rules: [{ required: true, message: '请选择推广地址！' }],
                     })(
                       <Select placeholder="请选择推广地址">
@@ -218,7 +320,20 @@ class MemberEditor extends Component {
                   </Form.Item>
                 ) : (
                   ''
-                )}
+                )} */}
+                <Form.Item {...formItemLayout} label="生效时间">
+                  {getFieldDecorator('effectiveDate', {
+                    rules: [{ required: true, message: '请选择生效时间！' }],
+                  })(
+                    <DatePicker
+                      format="YYYY-MM-DD HH:mm"
+                      showToday={false}
+                      disabledTime={disabledDateTime}
+                      showTime={{ defaultValue: moment('09:00', 'HH:mm') }}
+                      setFieldsValue={moment(new Date(), 'YYYY-MM-DD')}
+                    />
+                  )}
+                </Form.Item>
                 <Form.Item {...formItemLayout} label="推广码">
                   {getFieldDecorator('promoCode', {
                     initialValue: initialValue && initialValue.promoCode.split(' '),
